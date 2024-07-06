cb50bb31fbf347c3712e5b442df6dd0a20271e6b
Damilare Adepoju <adamilarej@gmail.com>
2024-07-06 10:55:17 +0100

    Refactor: Updated

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
-const mySwiper = new Swiper('.swiper-container', {
-    loop: true,
+const swiper = new Swiper(".mySwiper, .swiper-container", {
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+      renderBullet: function (index, className) {
+        return '<span class="' + className + '">' + "</span>";
+      },
+    },
     autoplay: {
         delay: 5000,
-        disableOnInteraction: false,
+        disableOnInteraction: true,
+    }
+  });
+
+  tailwind.config = {
+    theme: {
+        extend: {
+            fontFamily: {
+                'sans': ['Zurich', 'sans-serif'],
+                'ext': ['"Zurich Extended"', 'sans-serif'],
+            },
+            colors: {
+                'primary': '#F3FF34',
+            },
+        },
     },
-    pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
-    },
-});
+}

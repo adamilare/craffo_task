2123fc6a703f7edb2d861526f32b019171eddac5
Damilare Adepoju <adamilarej@gmail.com>
2024-07-06 10:14:56 +0100

    Initial setup

---
+++ index.js
@@ -0,0 +1,11 @@
+const mySwiper = new Swiper('.swiper-container', {
+    loop: true,
+    autoplay: {
+        delay: 5000,
+        disableOnInteraction: false,
+    },
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+});
